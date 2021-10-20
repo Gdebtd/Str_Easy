efd44920b9cfac24cc40dfdcc938933e33d218b1
Gdebtd <76738374+Gdebtd@users.noreply.github.com>
2021-10-20 21:35:48 +0300

    Update functions_3

--- functions_3.cpp
+++ functions_3.cpp
@@ -48,46 +48,83 @@ int itc_find_str(string str1, string str2)
             index = -1;
         }
         i++;
-		if (i2 > 0)
-		{
-			if (str2[i2 - 1] == str2[len2 - 1])
-				return index;
-		}
+	if (i2 > 0)
+	{
+	    if (str2[i2 - 1] == str2[len2 - 1])
+		return index;
+	}
     }
     return -1;
 }
 
 string itc_three_str(string str1, string str2, string str3)
 {
-	long long len = itc_len(str1);
-	long long len3 = itc_len(str3);
-	long long num = itc_find_str(str1, str2);
-	if (num == -1)
-		return str1;
-	string _str = "";
-	long long i = 0;
-	while (i < num)
-	{
-		_str += str1[i];
-		i++;
-	}
-	long long k = 0;
-	while (k < len3)
-	{
-		_str += str3[k];
-		k++;
-	}
-	i -= 1;
-	i += k;
-	while (i < len)
-	{
-		_str += str1[i];
-		i++;
-	}
-	return _str;
+    long long len = itc_len(str1);
+    long long len3 = itc_len(str3);
+    long long num = itc_find_str(str1, str2);
+    if (num == -1)
+	return str1;
+    string _str = "";
+    long long i = 0;
+    while (i < num)
+    {
+	_str += str1[i];
+	i++;
+    }
+    long long k = 0;
+    while (k < len3)
+    {
+	_str += str3[k];
+	k++;
+    }
+    i -= 1;
+    i += k;
+    while (i < len)
+    {
+	_str += str1[i];
+	i++;
+    }
+    return _str;
 }
 
 int itc_max_char_on_end(string str)
 {
-    return 0;
+    long long len = itc_len(str);
+    long long i = 0;
+    long long maxi = 0;
+    long long cnt = 0;
+    while (i < len)
+    {
+	if (str[i] >= '0' && str[i] <= '9')
+	{
+	    if (cnt == 0)
+	    {
+		cnt++;
+	    }
+	    else
+	    {
+		if (i > 0)
+		{
+		    if (str[i] - str[i - 1] == 1)
+			cnt++;
+		    else
+		    {
+			if (cnt > maxi)
+			    maxi = cnt;
+			cnt = 0;
+		    }
+		}
+	    }
+    }
+    else
+    {
+	if (cnt > maxi)
+	    maxi = cnt;
+	cnt = 0;
+    }
+    i++;
+    }
+    if (cnt > maxi)
+	maxi = cnt;
+    return maxi;
 }
